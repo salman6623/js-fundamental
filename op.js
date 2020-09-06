619de3da9ebbf14e1ff73414d31c6bb76052c0ea
salman6623 <ilhamsalman868@gmail.com>
2020-09-06 23:59:11 +0700

    first commit

---
+++ op.js
@@ -0,0 +1,8 @@
+const nama = 'ilham salman';
+let umur = 18;
+let tahunlahir = 2002;
+let hobi = 'jalan-jalan';
+let alamat = 'jaktim';
+let citaCita = 'bikin banga org tua';
+
+console.log(`${nama} umur ${umur} lahir ${tahunLahir}`)
